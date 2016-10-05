35b463029c85e125b56e4f69e811f7fb8763b04e
Arthur Melin <arthur.melin@epitech.eu>
2016-10-05 09:19:18 +0200

    Fix task 11

--- r_tacpy.sh
+++ r_tacpy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sed -n 2~2p | cut -d ":" -f 1 | rev | sort -r | sed -n ${MY_LINE1},${MY_LINE2}p | sed "s/$/, /" | tr -d "\n" | sed "s/, $/.\n/"
+sed -n 2~2p | cut -d ":" -f 1 | rev | sort -r | sed -n ${MY_LINE1},${MY_LINE2}p | sed "s/$/, /" | tr -d "\n" | sed "s/, $/./"
